64c90c362a04b23328c9c6fa74396dc5ee628f7b
Tiago <tiagoprysthon14@gmail.com>
2022-06-21 18:22:57 -0300

    Exercício 2 - feito

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Primeiros passos/exercicios.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Primeiros passos/exercicios.js
@@ -12,3 +12,12 @@ console.log (a-b)
 console.log (a*b)
 console.log (a/b)
 console.log (a%b)
+
+// 2 Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+const a = 10
+const b = 2
+if (a>b) {
+    console.log (a)
+} else {
+    console.log (b)
+}
