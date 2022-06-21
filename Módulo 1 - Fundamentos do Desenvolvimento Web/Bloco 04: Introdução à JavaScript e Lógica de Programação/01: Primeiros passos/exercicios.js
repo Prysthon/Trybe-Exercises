eea518a125d3765569e87a77a350b98da1248507
Tiago <tiagoprysthon14@gmail.com>
2022-06-21 19:15:39 -0300

    Exercício 8 - feito

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Primeiros passos/exercicios.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Primeiros passos/exercicios.js
@@ -116,3 +116,15 @@ if (grade < 0 || grade > 100) {
         console.log ("Nota: F")
     )
 }
+
+// 8. Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+const a = 90;
+const b = 45;
+const c = 5;
+
+let even = false
+
+if (a%2===0 || b%2===0 ||c%2===0) {
+    even = true;
+}
+console.log (even);
