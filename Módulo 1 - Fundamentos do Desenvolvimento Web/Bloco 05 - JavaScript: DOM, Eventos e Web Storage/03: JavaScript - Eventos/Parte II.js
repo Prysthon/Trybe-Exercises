59764f240da70e12c959e4d3330beb6c5f2cc4f0
Tiago <tiagoprysthon14@gmail.com>
2022-06-30 15:35:17 -0300

    Finalização Parte II

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/03: JavaScript - Eventos/Parte II.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/03: JavaScript - Eventos/Parte II.js
@@ -8,19 +8,49 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// R: Devido a classe "tech".
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function changeTech(event) {
+    let techDocument = document.getElementsByClassName('tech')[0];
+    techDocument.classList.remove ('tech');
+    event.target.classList.add ('tech')
+}
+
+firstLi.addEventListener('click', changeTech);
+secondLi.addEventListener('click', changeTech);
+thirdLi.addEventListener('click', changeTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+function inputText(event) {
+    let techDocument = document.getElementsByClassName('tech')[0];
+    let box = event.target.value;
+    techDocument.innerText = box;
+}
+
+input.addEventListener('keyup', inputText)
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+function myPlaylist () {
+    window.location.replace("https://open.spotify.com/artist/3WrFJ7ztbogyGnTHbHJFl2")
+}
 
+myWebpage.addEventListener('dblclick', myPlaylist)
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+function changeColor (event) {
+    event.target.style.background = 'green';
+}
+function changeColorBack (event) {
+    event.target.style.background = 'unset';
+}
+
+myWebpage.addEventListener('mouseover', changeColor)
+myWebpage.addEventListener('mouseout', changeColorBack)
 
 // Segue abaixo um exemplo do uso de event.target:
 
