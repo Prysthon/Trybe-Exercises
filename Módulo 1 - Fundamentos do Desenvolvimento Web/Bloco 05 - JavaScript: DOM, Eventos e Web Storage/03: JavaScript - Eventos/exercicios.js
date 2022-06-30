19a1ff428fdd31811784fc45292efdbeb4c8a6c1
Tiago <tiagoprysthon14@gmail.com>
2022-06-30 18:22:15 -0300

    E1 - Feito

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/03: JavaScript - Eventos/exercicios.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/03: JavaScript - Eventos/exercicios.js
@@ -13,4 +13,29 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  /* Exercício 1:
+  Crie um calendário dinamicamente.
+  O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+  Note que os dias 29 e 30 de novembro estão no array, pois representam respectivamente Domingo e Segunda-feira.
+   - A tag <ul> deve conter o id 'days';
+   - Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
+   - Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
+   - Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li> */
+
+   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+   function addDays () {
+    let ulTag = document.getElementById('days');
+    for (let day of decemberDaysList) {
+        let liTag = document.createElement('li');
+        liTag.className = 'day';
+        liTag.innerText = day;
+        if (day === 24 || day === 25 || day === 31) {
+            liTag.classList.add ('holiday');
+        } if (day === 4 || day === 11 || day === 18 || day === 25) {
+            liTag.classList.add ('friday');
+        }
+        ulTag.appendChild(liTag);
+    } 
+   }
+   addDays(decemberDaysList);
